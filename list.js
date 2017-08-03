3f6bbed80fe1ccfca8987d3b10ec5af5a4b65f1e
Michael  Sewell <mikesewell01@gmail.com>
2017-08-03 10:28:14 -0400

    test gitHub pages

--- list.js
+++ list.js
@@ -2,38 +2,57 @@
 
 // HTTP Request
 function loadData(){
-var userinput = document.querySelector('input').value;
+const userinput = document.querySelector('input').value;
+const searchSec = document.querySelector('.comicList');
+let input = encodeURI(userinput)
+var characterName = "";
+var charImaUrl = "";
+var chaSeries = "";
+var seriesArr = [];
+console.log(searchSec);
+console.log(input);
 
+https://gateway.marvel.com:443/v1/public/characters/hulk/comics?apikey=ee1f0c547b636265ac05222e1efd5e25
+
+// ajax Request
 var xhr = new XMLHttpRequest();
-xhr.open('GET','https://gateway.marvel.com:443/v1/public/characters?name=' + userinput +'&apikey=ee1f0c547b636265ac05222e1efd5e25',true);
+xhr.open('GET','https://gateway.marvel.com:443/v1/public/characters/' + input +'/comics?apikey=ee1f0c547b636265ac05222e1efd5e25',true);
 xhr.onload = function (){
-  var characterName = "";
-  var charImaUrl = "";
 
   var responseOject = JSON.parse(xhr.responseText);
+
   console.log(responseOject);
-  console.log();
   characterName = responseOject.data.results[0].name;
-  var characterImage = responseOject.data.results[0].thumbnail.path;
+  characterImage = responseOject.data.results[0].thumbnail.path;
   var imageX = responseOject.data.results[0].thumbnail.extension;
   charImaUrl = characterImage + "." + imageX;
   console.log(characterName);
   console.log(charImaUrl);
 
+
+
    for (var j = 0; j < responseOject.data.results[0].series.items.length; j++) {
-     var chaSeries = responseOject.data.results[0].series.items[j];
+     chaSeries = responseOject.data.results[0].series.items[j].name;
+     seriesArr.push(chaSeries);
      console.log(chaSeries);
    }
+   console.log(seriesArr);
+   // ===========================================
+  //  foreach(comic in chaSeries){
+   //
+  //    //searchSec.insertAdjacentHTML('afterbegin', '<div id="two">two</div>');
+  //  }
+
+
+
+   // ===========================================
 
 };
 xhr.send(null);
 }
 //window.addEventListener('load',loadData);
-var buttonSub = document.querySelector('.button');
-
-
-
-// buttonSub.addEventListener('click', function(){
-//   var userinput = document.querySelector('input').value;
-//   loadData();
-// }, false);
+const buttonSub = document.querySelector('form');
+buttonSub.addEventListener('submit',function(e){
+  e.preventDefault()
+  loadData();
+})
