ba42cfca0561da0940a408f45c22ee827bc0ef00
Michael  Sewell <mikesewell01@gmail.com>
2017-08-04 14:05:17 -0400

    completed project

--- list.js
+++ list.js
@@ -1,58 +1,99 @@
-//var userinput = "spider-man";
+//let userinput = "spider-man";
 
 // HTTP Request
-function loadData(){
-const userinput = document.querySelector('input').value;
-const searchSec = document.querySelector('.comicList');
-let input = encodeURI(userinput)
-var characterName = "";
-var charImaUrl = "";
-var chaSeries = "";
-var seriesArr = [];
-console.log(searchSec);
-console.log(input);
+function loadData() {
+  let userinput = document.querySelector("input").value
+  const searchSec = document.querySelector(".comicList")
+  let characterID = ""
+  let input = encodeURI(userinput)
+  let characterName = ""
+  let charImaUrl = ""
+  let chaSeries = ""
+  let seriesArr = []
+  let searchString =
+    "https://gateway.marvel.com:443/v1/public/characters?name=" +
+    input +
+    "&apikey=ee1f0c547b636265ac05222e1efd5e25"
 
-https://gateway.marvel.com:443/v1/public/characters/hulk/comics?apikey=ee1f0c547b636265ac05222e1efd5e25
+  // ajax Request
+  let xhr = new XMLHttpRequest()
+  xhr.open("GET", searchString, true)
+  xhr.onload = function() {
+    let responseOject = JSON.parse(xhr.responseText)
+    let characterVal = responseOject.data.count
+    let searchHead = document.querySelector("h2")
 
-// ajax Request
-var xhr = new XMLHttpRequest();
-xhr.open('GET','https://gateway.marvel.com:443/v1/public/characters/' + input +'/comics?apikey=ee1f0c547b636265ac05222e1efd5e25',true);
-xhr.onload = function (){
+    console.log(responseOject)
+    if (characterVal > 0) {
+      characterName = responseOject.data.results[0].name
+      characterID = responseOject.data.results[0].id
+      let characterImage = responseOject.data.results[0].thumbnail.path
+      let imageX = responseOject.data.results[0].thumbnail.extension
+      charImaUrl = characterImage + "." + imageX
+      searchHead.innerHTML = "Here are some comics from " + characterName
+      //let characterInsert = "<p id='char'><img id='char2' src=" + charImaUrl + "></p>"
+      //const characterDisplay = document.querySelector("h2")
+      //characterDisplay.insertAdjacentHTML("afterend", characterInsert)
+      console.log(characterName)
+      console.log(charImaUrl)
+      console.log(characterID)
+      comicPop()
 
-  var responseOject = JSON.parse(xhr.responseText);
+      let comics = []
+      let comicsImageUrl = []
+      function comicPop() {
+        let comicString =
+          "https://gateway.marvel.com:443/v1/public/characters/" +
+          characterID +
+          "/comics?apikey=ee1f0c547b636265ac05222e1efd5e25"
 
-  console.log(responseOject);
-  characterName = responseOject.data.results[0].name;
-  characterImage = responseOject.data.results[0].thumbnail.path;
-  var imageX = responseOject.data.results[0].thumbnail.extension;
-  charImaUrl = characterImage + "." + imageX;
-  console.log(characterName);
-  console.log(charImaUrl);
+        let comicXHR = new XMLHttpRequest()
+        comicXHR.open("GET", comicString, true)
+        comicXHR.onload = function() {
+          let comicOject = JSON.parse(comicXHR.responseText)
+          console.log(comicOject)
+          // characterName = comicOject.data.results[0].name;
 
-
-
-   for (var j = 0; j < responseOject.data.results[0].series.items.length; j++) {
-     chaSeries = responseOject.data.results[0].series.items[j].name;
-     seriesArr.push(chaSeries);
-     console.log(chaSeries);
-   }
-   console.log(seriesArr);
-   // ===========================================
-  //  foreach(comic in chaSeries){
-   //
-  //    //searchSec.insertAdjacentHTML('afterbegin', '<div id="two">two</div>');
-  //  }
-
-
-
-   // ===========================================
-
-};
-xhr.send(null);
+          for (let j = 0; j < comicOject.data.results.length; j++) {
+            comicTitle = comicOject.data.results[j].title
+            let comicImage = comicOject.data.results[j].thumbnail.path
+            let comicX = comicOject.data.results[j].thumbnail.extension
+            let comicLink = comicOject.data.results[j].urls[0].url
+            console.log(comicLink)
+            let comicImaUrl = comicImage + "." + comicX
+            let comicInsert =
+              "<li><img src=" +
+              comicImaUrl +
+              ' ><a href="' +
+              comicLink +
+              '">' +
+              comicTitle +
+              "</a></li>"
+            searchSec.insertAdjacentHTML("afterbegin", comicInsert)
+            comics.push(comicTitle)
+            comicsImageUrl.push(comicImaUrl)
+          }
+        }
+        comicXHR.send(null)
+      }
+      //var viewMoreBtn = document.querySelector("")
+      let viewMoreTxt =
+        '<p id="marvellink"><a href="https://www.marvel.com">View more at Marvel</a></p>'
+      searchSec.insertAdjacentHTML("afterend", viewMoreTxt)
+    } else {
+      var failTxt =
+        "<img id='failPic' class='failClass' src='./images/marvelgame_over.png'>"
+      searchHead.innerHTML = "Not a Marvel character, please try again!"
+      searchSec.insertAdjacentHTML("afterbegin", failTxt)
+    }
+  }
+  xhr.send(null)
 }
-//window.addEventListener('load',loadData);
-const buttonSub = document.querySelector('form');
-buttonSub.addEventListener('submit',function(e){
+const comicClear = document.querySelector(".comicList")
+
+const buttonSub = document.querySelector("form")
+buttonSub.addEventListener("submit", function(e) {
+  comicClear.innerHTML = ""
   e.preventDefault()
-  loadData();
+  loadData()
 })
